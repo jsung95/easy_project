8f05575de96d9d3929b5ac41e3742e6bc4f57112
jsung95 <76650814+jsung95@users.noreply.github.com>
2021-12-13 10:58:33 +0900

    로그인 구현 1차
    로그인 구현 1차

--- EasyProject/App.xaml.cs
+++ EasyProject/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using EasyProject.Model;
 
 namespace EasyProject
 {
@@ -13,5 +14,17 @@ namespace EasyProject
     /// </summary>
     public partial class App : Application
     {
+        public static NurseModel nurse_dto = null;
+
+        public App()
+        {
+            this.InitializeComponent();
+            nurse_dto = new NurseModel();
+            Console.WriteLine("===> Init NurseDTO");
+            Console.WriteLine("  Nurse NO : {0}", nurse_dto.Nurse_no);
+            Console.WriteLine("  Nurse NAME : {0}", nurse_dto.Nurse_name);
+            Console.WriteLine("  Nurse AUTH : {0}", nurse_dto.Nurse_auth);
+            Console.WriteLine("  DEPT ID : {0}", nurse_dto.Dept_id);
+        }//App Constructor
     }
 }
---
+++ EasyProject/Dao/ILoginDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyProject.Model;
+
+namespace EasyProject.Dao
+{
+    public interface ILoginDao
+    {
+        NurseModel LoginUserInfo(NurseModel nurse_dto);
+    }//interface
+
+}//namespace
