0abfa8f5ef57c2e7960698075e57b73b9de015e6
jsung95 <76650814+jsung95@users.noreply.github.com>
2021-12-13 17:46:40 +0900

    재고입력 - 수정2
    이제 ObservableCollection 리스트에 담겨요!

---
+++ EasyProject/Model/ProductForListModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyProject.Model
+{
+    public class ProductForListModel : Notifier
+    {
+        public string Prod_code { get; set; }
+        public string Prod_name { get; set; }
+        public string Category_name { get; set; }
+        public DateTime Prod_expire { get; set; }
+        public int? Prod_price { get; set; }
+        public int? Prod_total { get; set; }
+
+    }//class
+
+}//namespace
