a30192bbcad4abb3727a96c0b8e552bd5afb2bc0
bansongs <predicthappy@naver.com>
2021-12-20 16:29:18 +0900

    재고수정페이지 추가

---
+++ EasyProject/View/ModifyPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EasyProject.View
+{
+    /// <summary>
+    /// ModifyPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ModifyPage : Page
+    {
+        public ModifyPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void reset_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cancel_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void signUp_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
