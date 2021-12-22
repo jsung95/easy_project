9cb21294f156e7a14dc9d023e4a18ee7a0a84ad6
kimkangmin224 <kimkangmin224@gmail.com>
2021-12-22 17:42:48 +0900

    대시보드 추가
    대시보드 추가

--- EasyProject/ViewModel/ProductShowViewModel.cs
+++ EasyProject/ViewModel/ProductShowViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using Microsoft.Expression.Interactivity.Core;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace EasyProject.ViewModel
 {
@@ -45,6 +47,12 @@ namespace EasyProject.ViewModel
         public string SelectedSearchType { get; set; }
 
         //입력한 검색내용을 담을 프로퍼티
+
+        // 대시보드 프로퍼티
+        public ChartValues<int> Values { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public List<string> BarLabels { get; set; }       //string[]
+        public Func<double, string> Formatter { get; set; }
         private string textForSearch;
         public string TextForSearch 
         {
@@ -126,6 +134,8 @@ namespace EasyProject.ViewModel
 
             //App.xaml.cs 에 로그인할 때 바인딩 된 로그인 정보 객체
             Nurse = App.nurse_dto;
+
+            DashboardPrint();  //대시보드 프린트
         }
 
 
@@ -230,6 +240,42 @@ namespace EasyProject.ViewModel
             SelectedOutType = null;
             SelectedOutDept = null;
             InputOutCount = null;
+        }
+        public void DashboardPrint()                       //대시보드 출력(x축:제품code, y축:수량) 
+        {
+            SeriesCollection = new SeriesCollection();   //대시보드 틀
+
+
+            ChartValues<int> name = new ChartValues<int> { };            //y축들어갈 임시 값
+
+
+            List<ProductShowModel> list1 = product_dao.Prodtotal_Info();      //y축출력
+            //foreach (var item in list1)
+            //{
+            //    name.Add((int)item.Prod_total);
+            //}
+            for (int i = 0; i < 8; i++)
+            {
+                name.Add((int)list1[i].Prod_total);
+            }
+
+
+            Values = new ChartValues<int> { };
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "재고현황",   //+ i
+                Values = name,
+
+            });
+
+            BarLabels = new List<string>() { };                           //x축출력
+            List<ProductShowModel> list = product_dao.Prodcode_Info();
+            foreach (var item in list)
+            {
+                BarLabels.Add(item.Prod_code);
+            }
+
+            Formatter = value => value.ToString("N");   //문자열 10진수 변환
         }
 
 
