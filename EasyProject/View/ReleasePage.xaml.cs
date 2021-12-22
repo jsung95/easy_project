7e2b7ce893ab2f93260fe865afc172f6f7284137
jsung95 <76650814+jsung95@users.noreply.github.com>
2021-12-22 10:26:40 +0900

    재고 출고
    오류 수정

--- EasyProject/View/ReleasePage.xaml.cs
+++ EasyProject/View/ReleasePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EasyProject.ViewModel;
 
 namespace EasyProject.View
 {
@@ -51,13 +52,39 @@ namespace EasyProject.View
 
         private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(Type_comboBox.SelectedValue.Equals("사용") || Type_comboBox.SelectedValue.Equals("폐기"))
+            if(Type_comboBox.SelectedValue.Equals("사용"))
             {
                 Dept_comboBox.Visibility = Visibility.Hidden;
+
+                mount_TxtBox.Text = null;
+
+                mount_TxtBox_Hidden.IsEnabled = true;
+                mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
+            }
+            else if (Type_comboBox.SelectedValue.Equals("폐기"))
+            {
+                Dept_comboBox.Visibility = Visibility.Hidden;
+
+                mount_TxtBox.Text = Convert.ToString(ProductShowViewModel.SelectedProduct.Imp_dept_count);
+                mount_TxtBox.Focus();
+
+                mount_TxtBox_Hidden.Visibility = Visibility.Visible;
+                mount_TxtBox_Hidden.Text = Convert.ToString(ProductShowViewModel.SelectedProduct.Imp_dept_count);
+                mount_TxtBox_Hidden.IsEnabled = false;
+
+                Console.WriteLine("ori : " + mount_TxtBox.Text);
+                Console.WriteLine("ori enable? : " + mount_TxtBox.IsEnabled);
+                Console.WriteLine("aft : " + mount_TxtBox_Hidden.Text);
+                Console.WriteLine("aft enable? : " + mount_TxtBox_Hidden.IsEnabled);
             }
             else
             {
                 Dept_comboBox.Visibility = Visibility.Visible;
+
+                mount_TxtBox.Text = null;
+
+                mount_TxtBox_Hidden.IsEnabled = true;
+                mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
             }
             /*ComboBox currentComboBox = sender as ComboBox;
             
