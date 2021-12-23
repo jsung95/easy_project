6ee6566470d20259bcc7223a27dffaed25736a4b
jsung95 <76650814+jsung95@users.noreply.github.com>
2021-12-23 13:12:37 +0900

    Revert "Locator"
    This reverts commit c2b276c2d8cf3ea803c56374a3204c25e3e3e023.

--- EasyProject/Locator.cs
+++ EasyProject/Locator.cs
@@ -10,14 +10,7 @@ namespace EasyProject
 {
     public  class Locator
     {
-        public ProductViewModel ProductViewModel => Ioc.Default.GetService<ProductViewModel>();
-        public ProductShowViewModel ProductShowViewModel => Ioc.Default.GetService<ProductShowViewModel>();
-        public ProductInOutViewModel ProductInOutViewModel => Ioc.Default.GetService<ProductInOutViewModel>();
-        public LoginViewModel LoginViewModel => Ioc.Default.GetService<LoginViewModel>();
-        public PasswordChangeViewModel PasswordChangeViewModel => Ioc.Default.GetService<PasswordChangeViewModel>();
-        public UserAuthViewModel UserAuthViewModel => Ioc.Default.GetService<UserAuthViewModel>();
-        public OrderViewModel OrderViewModel => Ioc.Default.GetService<OrderViewModel>();
-        public SignupViewModel SignupViewModel => Ioc.Default.GetService<SignupViewModel>();
-
+        public ProductViewModel PVM=> Ioc.Default.GetService<ProductViewModel>();
+        public LoginViewModel LVM=> Ioc.Default.GetService<LoginViewModel>();
     }
 }
