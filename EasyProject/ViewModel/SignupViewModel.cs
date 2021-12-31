97a1308c4eccb2be23f140fa712ed8afab14aea6
jsung95 <76650814+jsung95@users.noreply.github.com>
2021-12-31 09:28:10 +0900

    회원가입 필수정보 기입 체크
    회원가입 필수정보 기입 체크

--- EasyProject/ViewModel/SignupViewModel.cs
+++ EasyProject/ViewModel/SignupViewModel.cs
@@ -73,59 +73,69 @@ namespace EasyProject.ViewModel
         public bool SignupInsert()
         {
             bool SignupResult;
-            Regex regex = new Regex(@"^[0-9]{8}$"); //아이디는 숫자 8자리
 
-            if(regex.IsMatch(Convert.ToString(Nurse.Nurse_no)))
+            if (Nurse.Nurse_name == null || Nurse.Nurse_no == null || Nurse.Nurse_pw == null || Nurse_RePw == null || SelectedDept.Dept_name == null)
             {
-                Nurse = dao.IdCheck(Nurse); //중복검사
-                Console.WriteLine("id reg ok");
-                regex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"); //비밀번호는 숫자,문자 조합
-                if (regex.IsMatch(Nurse.Nurse_pw))
+                MessageBox.Show("정보를 모두 입력해주세요.");
+                SignupResult = false;
+                return SignupResult;
+            }
+            else
+            {
+                Regex regex = new Regex(@"^[0-9]{8}$"); //아이디는 숫자 8자리
+                if (regex.IsMatch(Convert.ToString(Nurse.Nurse_no)))
                 {
-                    
-                    if (Nurse.Nurse_no != null) // 중복없을시 가입 진행
+                    Nurse = dao.IdCheck(Nurse); //중복검사
+                    Console.WriteLine("id reg ok");
+                    regex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$"); //비밀번호는 숫자,문자 조합
+                    if (regex.IsMatch(Nurse.Nurse_pw))
                     {
-                        if (Nurse.Nurse_pw.Equals(Nurse_RePw)) // 비밀번호, 비밀번호 재확인 값이 같다면 
-                        {
-                            dao.SignUp(Nurse, SelectedDept); //회원가입
-                            MessageBox.Show("회원가입 완료!");
-                            
-                            //회원가입을 1회 진행하면서 바인딩 되어서 남겨진 데이터 초기화
-                            Nurse.Nurse_no = null;
-                            Nurse.Nurse_name = null;
-                            SelectedDept = null;
 
-                            SignupResult = true;
-                            return SignupResult;
-                        }//if
-                        else // 입력한 두 비밀번호가 다르다면 
+                        if (Nurse.Nurse_no != null) // 중복없을시 가입 진행
                         {
-                            MessageBox.Show("비밀번호가 맞지 않습니다.");
+                            if (Nurse.Nurse_pw.Equals(Nurse_RePw)) // 비밀번호, 비밀번호 재확인 값이 같다면 
+                            {
+                                dao.SignUp(Nurse, SelectedDept); //회원가입
+                                MessageBox.Show("회원가입 완료!");
+
+                                //회원가입을 1회 진행하면서 바인딩 되어서 남겨진 데이터 초기화
+                                Nurse.Nurse_no = null;
+                                Nurse.Nurse_name = null;
+                                SelectedDept = null;
+
+                                SignupResult = true;
+                                return SignupResult;
+                            }//if
+                            else // 입력한 두 비밀번호가 다르다면 
+                            {
+                                MessageBox.Show("비밀번호가 맞지 않습니다.");
+                                SignupResult = false;
+                                return SignupResult;
+                            }//else                    
+                        }//if
+                        else // 중복 있으면 ( Nurse.Nurse_no가 dao.IdCheck(Nurse)에 의해 null 값임 )
+                        {
                             SignupResult = false;
                             return SignupResult;
-                        }//else                    
+                        }
+
                     }//if
-                    else // 중복 있으면 ( Nurse.Nurse_no가 dao.IdCheck(Nurse)에 의해 null 값임 )
+                    else
                     {
+                        MessageBox.Show("암호는 숫자, 문자 조합만 6자리 이상만 가능합니다.");
                         SignupResult = false;
                         return SignupResult;
-                    }
+                    }//else
 
                 }//if
                 else
                 {
-                    MessageBox.Show("암호는 숫자, 문자 조합만 6자리 이상만 가능합니다.");
+                    MessageBox.Show("사번은 숫자 8자리만 입력가능합니다.");
                     SignupResult = false;
                     return SignupResult;
                 }//else
-                
-            }//if
-            else
-            {
-                MessageBox.Show("사번은 숫자 8자리만 입력가능합니다.");
-                SignupResult = false;
-                return SignupResult;
-            }//else
+
+            }
 
         }//SignupResult
 
