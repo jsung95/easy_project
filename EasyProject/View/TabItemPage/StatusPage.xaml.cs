e513f01aae01c77be522935438efca7909b6a1e2
kim haeun <35323579+saeloi@users.noreply.github.com>
2022-01-03 12:58:34 +0900

    test
    test ---재고 현황 popupBox 부서 콤보박스 확인

--- EasyProject/View/TabItemPage/StatusPage.xaml.cs
+++ EasyProject/View/TabItemPage/StatusPage.xaml.cs
@@ -142,64 +142,65 @@ namespace EasyProject.View.TabItemPage
         {
             GraphCard.Visibility = Visibility.Visible;
         }
-        //private void reset_Btn_Click(object sender , RoutedEvent e)
-        //{
-        //    mount_TxtBox_Hidden.Text = "";
-        //}
+        private void reset_Btn_Click(object sender, RoutedEvent e)
+        {
+            //mount_TxtBox_Hidden.Text = "";
+        }
 
-        //private void cancel_Btn_Click(object sender, RoutedEventArgs e)
-        //{
+        private void cancel_Btn_Click(object sender, RoutedEventArgs e)
+        {
 
-        //}
+        }
 
-        //private void signUp_Btn_Click(object sender, RoutedEventArgs e)
-        //{
+        private void signUp_Btn_Click(object sender, RoutedEventArgs e)
+        {
 
-        //}
+        }
 
-        //private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        //{
-        //    var temp = Ioc.Default.GetService<ProductShowViewModel>();
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var temp = Ioc.Default.GetService<ProductShowViewModel>();
 
-        //    if (Type_comboBox.SelectedValue != null)
-        //    {
-        //        if (Type_comboBox.SelectedValue.Equals("사용"))
-        //        {
-        //            Dept_comboBox.Visibility = Visibility.Hidden;
+            //if (Type_comboBox.SelectedValue != null)
+            //{
+            //    if (Type_comboBox.SelectedValue.Equals("사용"))
+            //    {
+            //        Dept_comboBox.Visibility = Visibility.Hidden;
 
-        //            mount_TxtBox.Text = null;
+            //        mount_TxtBox.Text = null;
 
-        //            mount_TxtBox_Hidden.IsEnabled = true;
-        //            mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
-        //        }
-        //        else if (Type_comboBox.SelectedValue.Equals("폐기"))
-        //        {
-        //            Dept_comboBox.Visibility = Visibility.Hidden;
+            //        mount_TxtBox_Hidden.IsEnabled = true;
+            //        mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
+            //    }
+            //    else if (Type_comboBox.SelectedValue.Equals("폐기"))
+            //    {
+            //        Dept_comboBox.Visibility = Visibility.Hidden;
 
-        //            mount_TxtBox.Text = Convert.ToString(temp.SelectedProduct.Imp_dept_count);
-        //            mount_TxtBox.Focus();
+            //        mount_TxtBox.Text = Convert.ToString(temp.SelectedProduct.Imp_dept_count);
+            //        mount_TxtBox.Focus();
 
-        //            mount_TxtBox_Hidden.Visibility = Visibility.Visible;
-        //            mount_TxtBox_Hidden.Text = Convert.ToString(temp.SelectedProduct.Imp_dept_count);
-        //            mount_TxtBox_Hidden.IsEnabled = false;
+            //        mount_TxtBox_Hidden.Visibility = Visibility.Visible;
+            //        mount_TxtBox_Hidden.Text = Convert.ToString(temp.SelectedProduct.Imp_dept_count);
+            //        mount_TxtBox_Hidden.IsEnabled = false;
 
-        //            Console.WriteLine("ori : " + mount_TxtBox.Text);
-        //            Console.WriteLine("ori enable? : " + mount_TxtBox.IsEnabled);
-        //            Console.WriteLine("aft : " + mount_TxtBox_Hidden.Text);
-        //            Console.WriteLine("aft enable? : " + mount_TxtBox_Hidden.IsEnabled);
-        //        }
-        //        else
-        //        {
-        //            Dept_comboBox.Visibility = Visibility.Visible;
+            //        Console.WriteLine("ori : " + mount_TxtBox.Text);
+            //        Console.WriteLine("ori enable? : " + mount_TxtBox.IsEnabled);
+            //        Console.WriteLine("aft : " + mount_TxtBox_Hidden.Text);
+            //        Console.WriteLine("aft enable? : " + mount_TxtBox_Hidden.IsEnabled);
+            //    }
+            //    else
+            //    {
+            //        Dept_comboBox.Visibility = Visibility.Visible;
 
-        //            mount_TxtBox.Text = null;
+            //        mount_TxtBox.Text = null;
 
-        //            mount_TxtBox_Hidden.IsEnabled = true;
-        //            mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
-        //        }
-        //    }
-        //}
+            //        mount_TxtBox_Hidden.IsEnabled = true;
+            //        mount_TxtBox_Hidden.Visibility = Visibility.Hidden;
+            //    }
+            //}
+        }
 
 
     }
+
 }
