6e02ac8ba3791a452995e7c59038218e626a6cf8
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-10 21:47:15 +0900

    임시

--- EasyProject/View/SignupPage.xaml.cs
+++ EasyProject/View/SignupPage.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -90,5 +91,14 @@ namespace EasyProject
             }*/
 
         }
+
+        private void id_TxtBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+
     }
 }
