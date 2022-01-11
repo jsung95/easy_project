51e0896d97c785737a58e61a30a1d1d6dc0ff097
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-11 17:44:46 +0900

    app log

--- EasyProject/App.xaml.cs
+++ EasyProject/App.xaml.cs
@@ -23,7 +23,7 @@ namespace EasyProject
         
         protected override void OnStartup(StartupEventArgs e)
         {
-            log.Info("==== START UP EASY_PROJECT ====");
+            log.Info("OnStartup(StartupEventArgs) invoked.");
             base.OnStartup(e);
         }
 
@@ -31,6 +31,8 @@ namespace EasyProject
         public static CategoryModel category_dto = null;
         public App()
         {
+            log.Info("==== START UP EASY_PROJECT ====");
+            log.Info("Constructor App() invoked.");
             Ioc.Default.ConfigureServices(new ServiceCollection()
                 .AddSingleton<ProductViewModel>()
                 .AddSingleton<LoginViewModel>()
