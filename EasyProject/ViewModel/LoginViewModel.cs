d5a47001072d625742cbc756616f29cf449da1a1
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-11 17:57:46 +0900

    LoginViewModel LOG

--- EasyProject/ViewModel/LoginViewModel.cs
+++ EasyProject/ViewModel/LoginViewModel.cs
@@ -56,6 +56,7 @@ namespace EasyProject.ViewModel
 
         public LoginViewModel()
         {
+            log.Info("Constructor LoginViewModel() invoked.");
             messagequeue = new SnackbarMessageQueue();
             Nurse = new NurseModel();
         }
@@ -92,81 +93,108 @@ namespace EasyProject.ViewModel
         }
         public bool Login()
         {
-            log.Info("Start Login");
-            idCheckResult = dao.IdPasswordCheck(Nurse); // id/pw 가 일치하는 지 확인
-            Console.WriteLine("Login() idCheckResult: " + idCheckResult);
-            if (idCheckResult == true) // 일치할 경우
+            log.Info("Login() invoked.");
+
+            try
             {
-                Console.WriteLine("id password check ok!");
-                NurseModel result = dao.LoginUserInfo(Nurse); // 해당 사용자 정보를 NurseModel 객체에 넣는다.   
+                idCheckResult = dao.IdPasswordCheck(Nurse); // id/pw 가 일치하는 지 확인
+                Console.WriteLine("Login() idCheckResult: " + idCheckResult);
+                if (idCheckResult == true) // 일치할 경우
+                {
+                    Console.WriteLine("id password check ok!");
+                    NurseModel result = dao.LoginUserInfo(Nurse); // 해당 사용자 정보를 NurseModel 객체에 넣는다.   
 
-                dao.Login_Logging(result); //로깅 데이터도 추가
+                    dao.Login_Logging(result); //로깅 데이터도 추가
 
-                App.nurse_dto.Nurse_no = result.Nurse_no;
-                App.nurse_dto.Nurse_name = result.Nurse_name;
-                App.nurse_dto.Nurse_auth = result.Nurse_auth;
-                App.nurse_dto.Nurse_pw = result.Nurse_pw;
-                App.nurse_dto.Dept_id = result.Dept_id;
+                    App.nurse_dto.Nurse_no = result.Nurse_no;
+                    App.nurse_dto.Nurse_name = result.Nurse_name;
+                    App.nurse_dto.Nurse_auth = result.Nurse_auth;
+                    App.nurse_dto.Nurse_pw = result.Nurse_pw;
+                    App.nurse_dto.Dept_id = result.Dept_id;
 
-                log4net.GlobalContext.Properties["user_no"] = App.nurse_dto.Nurse_no;
-                log4net.GlobalContext.Properties["user_name"] = App.nurse_dto.Nurse_name;
-                log4net.GlobalContext.Properties["user_auth"] = App.nurse_dto.Nurse_auth;
-                log.Info("Login Sucess");
-                Console.WriteLine("로그인 성공");
-                Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
-                Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
-                Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
-                Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
-                Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
+                    log4net.GlobalContext.Properties["user_no"] = App.nurse_dto.Nurse_no;
+                    log4net.GlobalContext.Properties["user_name"] = App.nurse_dto.Nurse_name;
+                    log4net.GlobalContext.Properties["user_auth"] = App.nurse_dto.Nurse_auth;
+                    log.Info("Login Sucess");
+                    Console.WriteLine("로그인 성공");
+                    Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
+                    Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
+                    Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
+                    Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
+                    Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
 
-                return idCheckResult;
+                    return idCheckResult;
 
-            }
-            else
+                }//if
+                else
+                {
+                    Console.WriteLine("id password check fail!");
+                    Console.WriteLine("로그인 실패");
+                    Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
+                    Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
+                    Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
+                    Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
+                    Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
+
+                    return idCheckResult;
+                }//else
+            }//try
+            catch (Exception ex)
             {
-                Console.WriteLine("id password check fail!");
-                Console.WriteLine("로그인 실패");
-                Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
-                Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
-                Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
-                Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
-                Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
-
+                log.Error(ex.Message);
                 return idCheckResult;
-            }
-         
+            }//catch
         }//Login
 
         public void Logout()
         {
-            dao.Logout_Logging(App.nurse_dto); //로깅 데이터도 추가
+            log.Info("Logout() invoked.");
 
-            App.nurse_dto.Nurse_no = null;
-            App.nurse_dto.Nurse_name = null;
-            App.nurse_dto.Nurse_auth = null;
-            App.nurse_dto.Nurse_pw = null;
-            App.nurse_dto.Dept_id = null;
+            try
+            {
+                dao.Logout_Logging(App.nurse_dto); //로깅 데이터도 추가
 
-            log4net.GlobalContext.Properties["user_no"] = App.nurse_dto.Nurse_no;
-            log4net.GlobalContext.Properties["user_name"] = App.nurse_dto.Nurse_name;
-            log4net.GlobalContext.Properties["user_auth"] = App.nurse_dto.Nurse_auth;
+                App.nurse_dto.Nurse_no = null;
+                App.nurse_dto.Nurse_name = null;
+                App.nurse_dto.Nurse_auth = null;
+                App.nurse_dto.Nurse_pw = null;
+                App.nurse_dto.Dept_id = null;
 
-            Console.WriteLine("로그아웃 성공");
-            Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
-            Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
-            Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
-            Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
-            Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
-            
-            //로그아웃 버튼 클릭 시에 프로그램 재시작
-            System.Windows.Forms.Application.Restart();
-            System.Windows.Application.Current.Shutdown();
-        }
+                log4net.GlobalContext.Properties["user_no"] = App.nurse_dto.Nurse_no;
+                log4net.GlobalContext.Properties["user_name"] = App.nurse_dto.Nurse_name;
+                log4net.GlobalContext.Properties["user_auth"] = App.nurse_dto.Nurse_auth;
+
+                Console.WriteLine("로그아웃 성공");
+                Console.WriteLine("  Nurse NO : {0}", App.nurse_dto.Nurse_no);
+                Console.WriteLine("  Nurse NAME : {0}", App.nurse_dto.Nurse_name);
+                Console.WriteLine("  Nurse AUTH : {0}", App.nurse_dto.Nurse_auth);
+                Console.WriteLine("  Nurse PW : {0}", App.nurse_dto.Nurse_pw);
+                Console.WriteLine("  DEPT ID : {0}", App.nurse_dto.Dept_id);
+
+                //로그아웃 버튼 클릭 시에 프로그램 재시작
+                System.Windows.Forms.Application.Restart();
+                System.Windows.Application.Current.Shutdown();
+            }//try
+            catch(Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+
+        }//logout
 
         public void Logout_For_QuitProgram()
         {
-            dao.Logout_Logging(App.nurse_dto); //로깅 데이터도 추가
-        }
+            log.Info("Logout_For_QuitProgram");
+            try
+            {
+                dao.Logout_Logging(App.nurse_dto); //로깅 데이터도 추가
+            }//try
+            catch(Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+
+        }//Logout_For_QuitProgram
 
 
         private string password;
