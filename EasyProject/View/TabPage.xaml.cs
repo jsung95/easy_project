327401e85418195de61e91e0c8fbe0a03f0bc219
jsung95 <76650814+jsung95@users.noreply.github.com>
2022-01-12 09:12:50 +0900

    tabpage.xaml log

--- EasyProject/View/TabPage.xaml.cs
+++ EasyProject/View/TabPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation; using EasyProject.Dao;
+using log4net;
 
 namespace EasyProject
 {
@@ -23,10 +24,13 @@ namespace EasyProject
     /// </summary>
     public partial class TabPage : Page
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+
         private string CurrentButtonName = "StatusPageTabButton";
         DeptDao dept_dao = new DeptDao();
         public TabPage()
         {
+            log.Info("Constructor TapPage() invoked.");
             InitializeComponent();
             userNameTxtBox.Text = App.nurse_dto.Nurse_name;
             user_name.Text = App.nurse_dto.Nurse_name + "님";
@@ -45,91 +49,143 @@ namespace EasyProject
         }*/
         private void PageLoaded(object sender, RoutedEventArgs e)
         {
-            if (App.nurse_dto.Nurse_auth.Equals("NORMAL"))
-            {
-                InsertPageTabButton.Width = 0;
-                AuthorityPageTabButton.Width = 0;
-                GraphPageTabButton.Width = 0;
-                OrderPageTabButton.Width = 0;
-            }
-            else if (App.nurse_dto.Nurse_auth.Equals("ADMIN"))
-            {
-                AuthorityPageTabButton.Width = 0;
-                GraphPageTabButton.Width = 0;
-            }
-            else if (App.nurse_dto.Nurse_auth.Equals("SUPER"))
-            {
-                StatusPageTabButton.Width = 0;
-                GraphPageTabButton.Width = 0;
-                InsertPageTabButton.Width = 0;
-                IncomingOutgoingPageTabButton.Width = 0;
-                InsertPageTabButton.Width = 0;
-                OrderPageTabButton.Width = 0;
+            log.Info("PageLoaded(object, RoutedEventArgs) invoked.");
 
-                TabFrame.Source = new Uri("/View/TabItemPage/AuthorityPage.xaml", UriKind.Relative);
-            }
-        }
+            try
+            {
+                if (App.nurse_dto.Nurse_auth.Equals("NORMAL"))
+                {
+                    InsertPageTabButton.Width = 0;
+                    AuthorityPageTabButton.Width = 0;
+                    GraphPageTabButton.Width = 0;
+                    OrderPageTabButton.Width = 0;
+                }
+                else if (App.nurse_dto.Nurse_auth.Equals("ADMIN"))
+                {
+                    AuthorityPageTabButton.Width = 0;
+                    GraphPageTabButton.Width = 0;
+                }
+                else if (App.nurse_dto.Nurse_auth.Equals("SUPER"))
+                {
+                    StatusPageTabButton.Width = 0;
+                    GraphPageTabButton.Width = 0;
+                    InsertPageTabButton.Width = 0;
+                    IncomingOutgoingPageTabButton.Width = 0;
+                    InsertPageTabButton.Width = 0;
+                    OrderPageTabButton.Width = 0;
+
+                    TabFrame.Source = new Uri("/View/TabItemPage/AuthorityPage.xaml", UriKind.Relative);
+                }
+            }//try
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+
+        }//PageLoaded
 
         private void btn_close(object sender, RoutedEventArgs e)       //버튼 창닫기
         {
-            Window.GetWindow(this).Close();
-        }
+            log.Info("btn_close(object, RoutedEventArgs) invoked.");
+
+            try
+            {
+
+                Window.GetWindow(this).Close();
+            }//try
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+        }//btn_close
 
         private void TabButtonClick(object sender, RoutedEventArgs e)       //버튼 창닫기
         {
-            string buttonName = ((Button)e.Source).Name;
-            Button currentButton = (Button)this.FindName(buttonName);
+            log.Info("TabButtonClick(object, RoutedEventArgs) invoked.");
 
-            //GridCursor.Margin = new Thickness((150 * index), 0, 0, 0);
-            if (!buttonName.Equals(CurrentButtonName))
+            try
             {
-                var previousButton = (Button)this.FindName(CurrentButtonName);
-                previousButton.Background = null;
-            }
+                string buttonName = ((Button)e.Source).Name;
+                Button currentButton = (Button)this.FindName(buttonName);
 
-            BrushConverter bc = new BrushConverter();
+                //GridCursor.Margin = new Thickness((150 * index), 0, 0, 0);
+                if (!buttonName.Equals(CurrentButtonName))
+                {
+                    var previousButton = (Button)this.FindName(CurrentButtonName);
+                    previousButton.Background = null;
+                }
 
-            switch (buttonName)
+                BrushConverter bc = new BrushConverter();
+
+                switch (buttonName)
+                {
+                    case "StatusPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/StatusPage.xaml", UriKind.Relative);
+                        break;
+                    case "GraphPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/GraphTabPage.xaml", UriKind.Relative);
+                        break;
+                    case "InsertPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/InsertPage.xaml", UriKind.Relative);
+                        break;
+                    case "IncomingOutgoingPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/IncomingOutgoingPageBtn.xaml", UriKind.Relative);
+                        break;
+                    case "OrderPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/OrderPage.xaml", UriKind.Relative);
+                        break;
+                    case "AuthorityPageTabButton":
+                        TabFrame.Source = new Uri("TabItemPage/AuthorityPage.xaml", UriKind.Relative);
+                        break;
+
+                }
+
+                currentButton.Background = (Brush)bc.ConvertFrom("#F0EBE9");
+                CurrentButtonName = buttonName;
+            }//try
+            catch (Exception ex)
             {
-                case "StatusPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/StatusPage.xaml", UriKind.Relative);
-                    break;
-                case "GraphPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/GraphTabPage.xaml", UriKind.Relative);
-                    break;
-                case "InsertPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/InsertPage.xaml", UriKind.Relative);
-                    break;
-                case "IncomingOutgoingPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/IncomingOutgoingPageBtn.xaml", UriKind.Relative);
-                    break;
-                case "OrderPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/OrderPage.xaml", UriKind.Relative);
-                    break;
-                case "AuthorityPageTabButton":
-                    TabFrame.Source = new Uri("TabItemPage/AuthorityPage.xaml", UriKind.Relative);
-                    break;
+                log.Error(ex.Message);
+            }//catch
 
-            }
+        }//TabButtonClick
 
-            currentButton.Background = (Brush)bc.ConvertFrom("#F0EBE9");
-            CurrentButtonName = buttonName;
-        }
         private async void ButtonError_Click(object sender, RoutedEventArgs e)
         {
-            ErrorNotificationMessage msg = new ErrorNotificationMessage();
-            msg.Message = "로그아웃을 하시겠습니까?";
+            log.Info("ButtonError_Click(object, RoutedEventArgs)");
 
-            await DialogHost.Show(msg, "RootDialog");
-        }
+            try
+            {
+                ErrorNotificationMessage msg = new ErrorNotificationMessage();
+                msg.Message = "로그아웃을 하시겠습니까?";
+
+                await DialogHost.Show(msg, "RootDialog");
+            }//try
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+
+        }//ButtonError_Click
 
 
         private void pw_Change_Btn_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new Uri("/View/PasswordChangePage.xaml", UriKind.Relative));
-        }
-    }
-}
+            log.Info("pw_Change_Btn_Click(object, RoutedEventArgs) invoked.");
+
+            try
+            {
+
+                NavigationService.Navigate(new Uri("/View/PasswordChangePage.xaml", UriKind.Relative));
+            }//try
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }//catch
+        }//pw_Change_Btn_Click
+
+    }//class
+}//namespace
 
 
 
