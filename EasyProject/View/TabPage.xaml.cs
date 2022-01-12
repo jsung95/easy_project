f6a405316efd544e2e67c148456250a5f8101355
bansongs <predicthappy@naver.com>
2022-01-12 11:13:46 +0900

    Revert "log UI 추가"
    This reverts commit 7f963d0d3f32bfd69ea1932c366a26b9b0afc411.

--- EasyProject/View/TabPage.xaml.cs
+++ EasyProject/View/TabPage.xaml.cs
@@ -59,13 +59,11 @@ namespace EasyProject
                     AuthorityPageTabButton.Width = 0;
                     GraphPageTabButton.Width = 0;
                     OrderPageTabButton.Width = 0;
-                    LogPageTabButton.Width= 0;
                 }
                 else if (App.nurse_dto.Nurse_auth.Equals("ADMIN"))
                 {
                     AuthorityPageTabButton.Width = 0;
                     GraphPageTabButton.Width = 0;
-                    LogPageTabButton.Width = 0;
                 }
                 else if (App.nurse_dto.Nurse_auth.Equals("SUPER"))
                 {
@@ -138,9 +136,6 @@ namespace EasyProject
                         break;
                     case "AuthorityPageTabButton":
                         TabFrame.Source = new Uri("TabItemPage/AuthorityPage.xaml", UriKind.Relative);
-                        break;
-                    case "LogPageTabButton":
-                        TabFrame.Source = new Uri("TabItemPage/LogStatusPageBtn.xaml", UriKind.Relative);
                         break;
 
                 }
